b92fab158ad1a87ffff65049ffa4a94e217ffbed
Choene Chokoe <choene.chokoe@cyintecs.co.za>
2024-12-24 17:02:59 +0200

    Update serve directory

--- student-portal-frontend/server.js
+++ student-portal-frontend/server.js
@@ -6,10 +6,10 @@ console.log('Current directory:', __dirname);
 console.log('Files in current directory:', require('fs').readdirSync(__dirname));
 console.log('Files in dist:', require('fs').readdirSync(path.join(__dirname, 'dist')));
 
-app.use('/', express.static(path.join(__dirname, 'dist/student-portal-frontend/browser')));
+app.use('/', express.static(path.join(__dirname, 'dist/browser')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/student-portal-frontend/browser/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/browser/index.html'));
 });
 
 const port = process.env.PORT || 3000;
